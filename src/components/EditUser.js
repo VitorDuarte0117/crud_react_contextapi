360fb7c5c4a3e7fe3162caafd88bcc3853ee50af
Vitor <vitorduarte.vds7@gmail.com>
2022-04-15 19:16:05 -0300

    features

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,14 +1,37 @@
-import React from "react";
-
-import { Link } from "react-router-dom";
+import React, { useContext, useState, useEffect } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Link, useNavigate } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
-export const EditUser = () => {
+export const EditUser = (props) => {
+    const [selectedUser, setSelectedUser] = useState({
+        id: "",
+        name: "",
+    });
+    const { users, editUser } = useContext(GlobalContext);
+    const navigate = useNavigate();
+    const currentUserId = props.match.params.id;
+
+    useEffect(() => {
+        const userId = currentUserId;
+        const selectedUser = users.find((user) => user.id === userId);
+        setSelectedUser(selectedUser);
+    });
+
+    const onSubmit = () => {
+        navigate("/");
+    };
+    const onChange = (e) => {};
     return (
-        <Form>
+        <Form onSubmit={onSubmit}>
             <FormGroup>
                 <Label>Edit User</Label>
-                <Input placeholder="Enter a user name" type="text"></Input>
+                <Input
+                    placeholder="Enter a user name"
+                    type="text"
+                    onChange={onChange}
+                ></Input>
             </FormGroup>
             <Button type="submit" color="success">
                 Edit
