2e4ab636d2bc638ccddf0347123fa6d9d42c349c
Vitor <vitorduarte.vds7@gmail.com>
2022-04-16 14:28:17 -0300

    almost finished

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,41 +1,48 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import { Link, useNavigate } from "react-router-dom";
-import { v4 as uuid } from "uuid";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export const EditUser = (props) => {
+    const { editUser, users } = useContext(GlobalContext);
     const [selectedUser, setSelectedUser] = useState({
         id: "",
         name: "",
     });
-    const { users, editUser } = useContext(GlobalContext);
+
     const navigate = useNavigate();
-    const currentUserId = props.match.params.id;
+    const currentUserId = props.id;
 
     useEffect(() => {
         const userId = currentUserId;
         const selectedUser = users.find((user) => user.id === userId);
         setSelectedUser(selectedUser);
-    });
+    }, [currentUserId, users]);
 
-    const onSubmit = () => {
+    const onChange = (e) => {
+        setSelectedUser({ ...selectedUser, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        editUser(selectedUser);
         navigate("/");
     };
-    const onChange = (e) => {};
+
     return (
         <Form onSubmit={onSubmit}>
             <FormGroup>
-                <Label>Edit User</Label>
+                <Label>Name</Label>
                 <Input
-                    placeholder="Enter a user name"
                     type="text"
                     onChange={onChange}
+                    value={selectedUser.name}
+                    name="name"
+                    placeholder="Enter user"
+                    required
                 ></Input>
             </FormGroup>
-            <Button type="submit" color="success">
-                Edit
-            </Button>
+            <Button type="submit">Edit Name</Button>
             <Link to="/" className="btn btn-danger ml-2">
                 Cancel
             </Link>
